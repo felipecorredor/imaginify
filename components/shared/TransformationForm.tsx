575e1b17f177e0d02231a6759b7a2f53ba4a8456
Felipe <juan.corredor@wizeline.com>
2024-03-30 10:12:06 -0500

    Transformation Type with Custom fields

---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
+import { defaultValues } from "@/constants";
+import { CustomField } from "./CustomField";
+import { Input } from "../ui/input";
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+
+const TransformationForm = ({
+  action,
+  data = null,
+}: TransformationFormProps) => {
+  const initialValues =
+    data && action === "Update"
+      ? {
+          title: data?.title,
+          aspectRatio: data?.aspectRatio,
+          color: data?.color,
+          prompt: data?.prompt,
+          publicId: data?.publicId,
+        }
+      : defaultValues;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          control={form.control}
+          render={({ field }) => <Input {...field} className="input-field" />}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default TransformationForm;
