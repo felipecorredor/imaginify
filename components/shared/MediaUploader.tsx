c20f808dd9eb238e8b7299d80ad8d71dd8a9b423
Felipe <juan.corredor@wizeline.com>
2024-04-08 22:50:08 -0500

    Update Image Transformation and Home Page

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,12 +1,17 @@
+"use client";
+import Image from "next/image";
 import { useToast } from "../ui/use-toast";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
+import React from "react";
 
 interface MediaUploaderProps {
   type: string;
   onValueChange: (value: string) => void;
-  setImage: (value: string) => void;
+  setImage: React.Dispatch<any>;
   publicId: string;
-  image: string;
+  image: any;
 }
 
 const MediaUploader = ({
@@ -19,6 +24,16 @@ const MediaUploader = ({
   const { toast } = useToast();
 
   const onUploadSuccessHandler = (result: any) => {
+    setImage((prevState: any) => ({
+      ...prevState,
+      publicId: result?.info?.public_id,
+      width: result?.info?.width,
+      height: result?.info?.height,
+      secureURL: result?.info?.secure_url,
+    }));
+
+    onValueChange(result.info?.public_id);
+
     toast({
       title: "Image uploaded successfully",
       description: "1 credit was deducted from your account",
@@ -44,7 +59,36 @@ const MediaUploader = ({
       onError={onUploadErrorHandler}
     >
       {({ open }) => {
-        return <div className="flex flex-col"></div>;
+        return (
+          <div className="flex flex-col gap-4">
+            <h3 className="h3-bold text-dark-600">Original</h3>
+            {publicId ? (
+              <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                <CldImage
+                  width={getImageSize(type, image, "width")}
+                  height={getImageSize(type, image, "height")}
+                  src={publicId}
+                  alt="Image"
+                  sizes={"(max-width: 767px) 100vw, 50vw"}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className="media-uploader_cldImage"
+                />
+              </div>
+            ) : (
+              <div className="media-uploader_cta" onClick={() => open()}>
+                <div className="media-uploader_cta-image">
+                  <Image
+                    src="/assets/icons/add.svg"
+                    alt="Add Image"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+                <p className="p-14-medium">Click here to upload image</p>
+              </div>
+            )}
+          </div>
+        );
       }}
     </CldUploadWidget>
   );
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -6,6 +6,7 @@ import { z } from "zod";
 import { Form } from "@/components/ui/form";
 import {
   aspectRatioOptions,
+  creditFee,
   defaultValues,
   transformationTypes,
 } from "@/constants";
@@ -19,11 +20,17 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { Button } from "../ui/button";
-import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
+import { updateCredits } from "@/lib/actions/user.actions";
+import { headers } from "next/headers";
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -36,6 +43,7 @@ export const formSchema = z.object({
 const TransformationForm = ({
   action,
   data = null,
+  userId,
   type,
   creditBalance,
   config = null,
@@ -51,6 +59,7 @@ const TransformationForm = ({
   const [transformationConfig, setTransformationConfig] = useState(config);
 
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   const initialValues =
     data && action === "Update"
@@ -68,9 +77,68 @@ const TransformationForm = ({
     defaultValues: initialValues,
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsSubmitting(true);
+
+    if (data || image) {
+      const transformationUrl = getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      });
+
+      const imageData = {
+        title: values.title,
+        publicId: image?.publicId,
+        transformationType: type,
+        width: image?.width,
+        height: image?.height,
+        config: transformationConfig,
+        secureURL: image?.secureURL,
+        transformationURL: transformationUrl,
+        aspectRatio: values.aspectRatio,
+        prompt: values.prompt,
+        color: values.color,
+      };
+
+      if (action === "Add") {
+        try {
+          const newImage = await addImage({
+            image: imageData,
+            userId,
+            path: "/",
+          });
+
+          if (newImage) {
+            form.reset();
+            setImage(data);
+            router.push(`/transformations/${newImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      if (action === "Update") {
+        try {
+          const updatedImage = await updateImage({
+            image: { ...imageData, _id: data._id },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+            router.push(`/transformations/${updatedImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    setIsSubmitting(false);
+  };
 
   const onSelectFieldHandler = (
     value: string,
@@ -103,10 +171,11 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }));
-    }, 1000);
+    }, 1000)();
+
+    return onChangeField(value);
   };
 
-  // TODO: Return to updateCredits
   const onTransFormationHandler = async () => {
     setIsTransforming(true);
 
@@ -117,13 +186,20 @@ const TransformationForm = ({
     setNewTransformation(null);
 
     startTransition(async () => {
-      // await updateCredits(userId, creditFee)
+      await updateCredits(userId, creditFee);
     });
   };
+
+  useEffect(() => {
+    if (image && (type === "restore" || type === "removeBackground")) {
+      setNewTransformation(transformationType.config);
+    }
+  }, [image, transformationType.config, type]);
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
           name="title"
           formLabel="Image Title"
@@ -143,6 +219,7 @@ const TransformationForm = ({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select size" />
@@ -223,6 +300,15 @@ const TransformationForm = ({
                 type={type}
               />
             )}
+          />
+
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
           />
         </div>
 
